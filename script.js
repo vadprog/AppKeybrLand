108fab9b5d713b143b3faf874e8adc583f2e59fe
vadprog <vadgtd@gogle.com>
2021-01-31 12:57:23 +0300

    span and timer

--- script.js
+++ script.js
@@ -1,6 +1,33 @@
 const RANDOM_QUOTE_API_URL = 'http://api.quotable.io/random' // запрос рандомных фраз
 const quoteDisplayElement = document.getElementById('quoteDisplay')
 const quoteInputElement = document.getElementById('quoteInput')
+const timerElement = document.getElementById('timer')
+
+quoteInputElement.addEventListener('input', () => {
+    //console.log('changed')
+    const arrayQuote = quoteDisplayElement.querySelectorAll('span')
+    const arrayValue = quoteInputElement.value.split('')
+
+    let correct = true
+    arrayQuote.forEach((characterSpan, index) => {
+        const character = arrayValue[index]
+        console.log(character)
+
+        if (character == null) {
+            characterSpan.classList.remove('correct')
+            characterSpan.classList.remove('incorrect')
+            correct = false
+        } else if (character === characterSpan.innerText) {
+            characterSpan.classList.add('correct')
+            characterSpan.classList.remove('incorrect')
+        } else {
+            characterSpan.classList.remove('correct')
+            characterSpan.classList.add('incorrect')
+            correct = false
+        }
+    })
+    if (correct) renderNewQuote()
+})
 
 // создать функцию по работе с фразами
 function getRandomQuote () {
@@ -22,10 +49,37 @@ async function renderNewQuote() {
     quoteDisplayElement.innerHTML = ''
     quote.split('').forEach(character => {
         const characterSpan = document.createElement('span')
+        // characterSpan.classList.add('correct') // класс в дом для проверки корректного ввода 
+        // characterSpan.classList.add('incorrect') // класс в дом для проверки не корректного ввода 
         characterSpan.innerText = character
         quoteDisplayElement.appendChild(characterSpan)
     });
     quoteInputElement.value = null
+    startTimer()
 }
 
+let startTime
+function startTimer() {
+   timerElement.innerText = 0
+   startTime = new Date ()
+   setInterval(() => {
+       timer.innerText = getTimerTime()
+   }, 1000)
+   
+}
+
+
+
+function getTimerTime() {
+    return Math.floor((new Date() - startTime) / 1000)
+}   
+
+
+//   if (startTime = 100) {
+//     renderNewQuote()
+// }  
+
 renderNewQuote()
+
+// обновление фразы по времени
+// обновление фразы по равному количеству символов
